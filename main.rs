ad4c8fb5dc843f84fbcb38d42f7c1e23f5816d96
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2024-11-01 06:56:47 +0100

    refactor: separate into xor.rs file

--- main.rs
+++ main.rs
@@ -1,58 +1,15 @@
-use std::collections::HashMap;
-use serde::Serialize;
 use std::env;
+use xor::{Xor, get_bip39_index_by_word, xor_bits};
+mod xor;
 
-#[derive(Debug, Serialize)]
-struct Xor {
-    word_a: String,
-    word_b: String,
-    xor_word: String,
-}
-
-fn create_word_to_index_map(words: &[&str]) -> HashMap<String, u16> {
-    words
-        .iter()
-        .enumerate()
-        .map(|(i, &word)| (word.to_string(), i as u16))
-        .collect()
-}
-
-fn word_to_bits(word_to_index: &HashMap<String, u16>, word: &str) -> u16 {
-    match word_to_index.get(word) {
-        Some(&index) => index,
-        None => {
-            eprintln!("Word '{}' not found in BIP39 list", word);
-            std::process::exit(1);
-        }
-    }
-}
-
-fn xor_bits(bits1: u16, bits2: u16) -> u16 {
-    bits1 ^ bits2
-}
-
-fn get_bip39_index_by_word(words: &[&str], word: &str) -> u16 {
-    let word_to_index = create_word_to_index_map(&words);
-    let bits = word_to_bits(&word_to_index, &word);
-    bits
-}
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        eprintln!("Usage: {} <word1> <word2>", args[0]);
-        std::process::exit(1);
-    }
-
-    let word_a = args[1].to_string();
-    let word_b = args[2].to_string();
+fn get_xor(word_a: &str, word_b: &str) -> Vec<Xor> {
 
     let bip39_words = include_str!("bip39-english.txt")
         .lines()
         .collect::<Vec<_>>();
 
-    let bits1 = get_bip39_index_by_word(&bip39_words, &word_a.as_str());
-    let bits2 = get_bip39_index_by_word(&bip39_words, &word_b.as_str());
+    let bits1 = get_bip39_index_by_word(&bip39_words, &word_a);
+    let bits2 = get_bip39_index_by_word(&bip39_words, &word_b);
 
     let xor_result = xor_bits(bits1, bits2);
 
@@ -66,13 +23,29 @@ fn main() {
 
     let mut xor_result = vec![];
     xor_result.push(Xor {
-        word_a,
-        word_b,
+        word_a: word_a.to_string(),
+        word_b: word_b.to_string(),
         xor_word: xor_word.to_string(),
     });
+    xor_result
+}
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Usage: {} <word1> <word2>", args[0]);
+        std::process::exit(1);
+    }
+
+    let word_a = &args[1];
+    let word_b = &args[2];
+
+    let xor_result = get_xor(word_a.as_str(), word_b.as_str());
 
     println!("Xor: {}", serde_json::to_string_pretty(&xor_result).unwrap());
 }
+
 
 #[cfg(test)]
 mod tests {
---
+++ xor.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub struct Xor {
+    pub word_a: String,
+    pub word_b: String,
+    pub xor_word: String,
+}
+
+fn create_word_to_index_map(words: &[&str]) -> HashMap<String, u16> {
+    words
+        .iter()
+        .enumerate()
+        .map(|(i, &word)| (word.to_string(), i as u16))
+        .collect()
+}
+
+fn word_to_bits(word_to_index: &HashMap<String, u16>, word: &str) -> u16 {
+    match word_to_index.get(word) {
+        Some(&index) => index,
+        None => {
+            eprintln!("Word '{}' not found in BIP39 list", word);
+            std::process::exit(1);
+        }
+    }
+}
+
+pub fn xor_bits(bits1: u16, bits2: u16) -> u16 {
+    bits1 ^ bits2
+}
+
+pub fn get_bip39_index_by_word(words: &[&str], word: &str) -> u16 {
+    let word_to_index = create_word_to_index_map(&words);
+    let bits = word_to_bits(&word_to_index, &word);
+    bits
+}
